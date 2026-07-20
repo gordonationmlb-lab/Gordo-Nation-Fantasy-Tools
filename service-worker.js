1a2b22e5259acaa32ba52721b0efc0765874ac61
gordonationmlb-lab <gordonationmlb@gmail.com>
2026-07-20 11:16:07 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'gordo-calc-v46-2026-07-08';
+const CACHE = 'gordo-calc-v47b-2026-07-20';
 const SHELL = [
   './', 'index.html', 'manifest.json',
   'icon-192.png', 'icon-512.png', 'icon-512-maskable.png', 'apple-touch-icon.png'
